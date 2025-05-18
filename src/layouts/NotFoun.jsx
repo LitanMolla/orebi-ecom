8b9f75503928b1d3235c15e521a6be518968ac59
LitanMolla <litanmolla9@gmail.com>
2025-05-19 05:59:02 +0600

    404 page add

--- src/layouts/NotFoun.jsx
+++ src/layouts/NotFoun.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
-import Image from '../components/Image'
-import notFound from '../assets/images/notFound.png'
+import { FaSearch } from "react-icons/fa";
 import Container from '../components/Container'
+import Btn from "../components/Btn";
+import { Link } from "react-router-dom";
 
 const NotFoun = () => {
   return (
     <>
         <Container>
-        <div className="w-1/2 m-auto my-20">
-        <Image src={notFound} className={'w-full'}/>
-        </div>
+          <div className="w-1/2 mt-21 mb-35 space-y-12">
+            <h2 className='font-bold text-[200px]'>404</h2>
+          <p className='text-base text-[#767676]'>The page you were looking for couldn't be found. The page could be removed or you misspelled the word while searching for it. Maybe try a search?</p>
+            <div className="relative">
+              <input type="text" placeholder='Type to search' className='p-5 border border-[#767676]/50 w-full rounded-md outline-0' />
+              <FaSearch className="absolute right-4 top-1/2 -translate-y-1/2  "/>
+            </div>
+          <Link to={'/'}><Btn text={'Back to Home'} /></Link>
+          </div>
         </Container>
     </>
   )
